7538acb6fab448f32ea8e11c8da5c080698958c8
MubarekUyghurturk <mubarek.ehmat@gmail.com>
2021-10-03 13:43:54 -0700

    delete tests package

--- src/tests/WoodenSpoonTest.java
+++
@@ -1,9 +0,0 @@
-package tests;
-
-public class WoodenSpoonTest {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Wooden Spoon");
-        }
-    }
-}
