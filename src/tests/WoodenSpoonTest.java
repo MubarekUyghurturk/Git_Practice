51968b2b415211233a025d9a8f4c6036591a1b1b
MubarekUyghurturk <mubarek.ehmat@gmail.com>
2021-10-03 13:30:05 -0700

    add woodenSpoon test

---
+++ src/tests/WoodenSpoonTest.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class WoodenSpoonTest {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Wooden Spoon");
+        }
+    }
+}
